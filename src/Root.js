7631df6754c0e441915adac8b05f38bf5e602f31
iNata <natandreychenko@gmail.com>
2017-10-05 14:14:15 +0300

    ADD redux, redux-saga middleware, fetchData example

--- src/Root.js
+++ src/Root.js
@@ -1,27 +1,88 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { TouchableHighlight, View, Text, StyleSheet } from 'react-native';
 
-export default class Root extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Hi, there!</Text>
-                <Text>Open up App.js to start working on your app!</Text>
-                <Text>Changes you make will automatically reload.</Text>
-                <Text>Shake your phone to open the developer menu.</Text>
-            </View>
-        );
+import { connect } from 'react-redux';
+import { fetchData } from './Actions/index';
+
+let styles;
+
+const Root = (props) => {
+    const {
+        container,
+        text,
+        button,
+        buttonText,
+        mainContent,
+        mainContentText
+    } = styles;
+
+    return (
+        <View style={container}>
+            <Text style={text}>Redux Examples</Text>            
+            <TouchableHighlight style={button} onPress={() => props.fetchData()}>
+                <Text style={buttonText}>Load Data</Text>
+            </TouchableHighlight>
+            <View style={mainContent}>
+                {
+                    props.appData.isFetching && <Text>Loading</Text>
+                }
+                {
+                    props.appData.data.length ? (
+                      props.appData.data.map((person, i) => {
+                        return <View key={i} style={mainContentText}>
+                          <Text>Name: {person.name}</Text>
+                          <Text>Age: {person.age}</Text>
+                        </View>
+                      })
+                    ) : null
+                }
+             </View>
+        </View>
+    )
+};
+
+styles = StyleSheet.create({
+    container: {
+        marginTop: 100
+    },
+    text: {
+        textAlign: 'center'
+    },
+    button: {
+        height: 60,
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#0b7eff'
+    },
+    buttonText: {
+        color: 'white'
+    },
+    mainContent: {
+        margin: 10,
+        alignItems: 'center'    
+    },
+    mainContentText: {
+        alignItems: 'center'
+    }
+});
+
+function mapStateToProps (state) {
+    return {
+        appData: state.appData
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+function mapDispatchToProps (dispatch) {
+    return {
+        fetchData: () => dispatch(fetchData())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Root)
 
 
 
---
+++ src/Stores/configureStore.js
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware } from 'redux';
+
+import app from '../Reducers/index';
+
+import createSagaMiddleware from 'redux-saga';
+
+import dataSaga from '../Saga/index';
+
+const sagaMiddleware = createSagaMiddleware();
+
+export default function configureStore() {
+    const store = createStore(app, applyMiddleware(sagaMiddleware));
+    sagaMiddleware.run(dataSaga);
+    return store
+}
